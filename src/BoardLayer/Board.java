61c7e74c6fc7c04d026b9df98b38106d3b1f0f1f
Edvan Leite <Leite.edvan28@gmail.com>
2024-08-05 13:17:43 -0300

    BoardException e Programacao Defensiva

--- src/BoardLayer/Board.java
+++ src/BoardLayer/Board.java
@@ -1,5 +1,7 @@
 package BoardLayer;
 
+import javax.swing.text.Position;
+
 public class Board {
 
 	
@@ -8,6 +10,9 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
+		if (rows<1 || columns < 1) {
+			throw new BordException("Erro Criando Tabuleiro. Eh necessatio que tenha pelo menos 1 linha e 1 coluna");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -32,20 +37,47 @@ public class Board {
 	
 	
 	public Piece  piece(int rows, int columns) {
+		if(!positionExists(rows,columns)) {
+			throw new BordException ("Possicao Inexistente");
+		}
 		return  pieces[rows][columns];
 	}
 	
 	public Piece  piece(position pos ) {
+		if(!positionExists(pos)) {
+			throw new BordException ("Possicao Inexistente");
+		}
 		return  pieces[pos.getRow()][pos.getColumn()];
 	}
 	
-	
-	
-	
 	public void PlacePiece (Piece piece, position position) {
+		if(TheresIsAPiece(position)) {
+			throw new BordException ("Possicao Ja Ocupada");
+		}
+		
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 		
 	}
 	
+	
+	public boolean positionExists (int row, int column) {
+		return row >=0 && row < rows    &&  column >= 0 && column < columns;
+		
+	}
+	
+	public boolean positionExists (position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	
+	
+	public boolean TheresIsAPiece(position position) {
+		if(!positionExists(position)) {
+			throw new BordException ("Possicao Inexistente");
+		}
+		
+		return piece(position) != null;
+				
+	}
 }
---
+++ src/BoardLayer/BordException.java
@@ -0,0 +1,13 @@
+package BoardLayer;
+
+public class BordException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public BordException(String msg) {
+		super(msg);
+	}
+	
+	
+
+	
+}
